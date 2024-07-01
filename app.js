02dbbc9485e5a9a62c7027345c58b2ecbd3b3827
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2024-07-01 15:14:02 +0300

    Fixing location bugs

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const getApproximateLocationFromIp = require('./getLocationFromIp');
 const getVisitorInfo = async (req) => {
     const parseUrl = url.parse(req.url, true);
     const visitorName = parseUrl.query.visitor_name;
-    // const clientIp = '85.25.43.84';
     let client_ip = getClientIp(req);
     let location = 'New York';
 
@@ -15,16 +14,20 @@ const getVisitorInfo = async (req) => {
             client_ip = '127.0.0.1';
             location = 'New York';
         } else {
-            location = await getApproximateLocationFromIp(client_ip);
+            const locationInfo = await getApproximateLocationFromIp(client_ip);
+            if (locationInfo && locationInfo.city && locationInfo.city.names && locationInfo.city.names.en) {
+                location = locationInfo.city.names.en;
+            } else {
+                location = 'New York'; // Fallback location
+            }
         }
-        // const visitorLocation = await getApproximateLocationFromIp(clientIp);
-        // const visitorLocation = 'New York';
+
         const visitorTemperature = await getWeather(location);
 
         return {
             client_ip,
             location,
-            greeting: `Hello, ${visitorName}!, the temperature is ${visitorTemperature} degrees Celcius in ${location}`,
+            greeting: `Hello, ${visitorName}! The temperature is ${visitorTemperature} degrees Celsius in ${location}`,
         };
     } catch (error) {
         throw new Error(`Failed to get visitor info: ${error.message}`);
--- getLocationFromIp.js
+++ getLocationFromIp.js
@@ -4,14 +4,13 @@ const accountId = 1032814;
 const licenseKey = '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';
 
 const getApproximateLocationFromIp = async (ip) => {
-    console.log(`Getting location from IP: ${ip}`);
     const client = new WebServiceClient(accountId, licenseKey, {
         host: 'geolite.info',
     });
 
     try {
         const response = await client.city(ip);
-        return response.city.names.en;
+        return response;
     } catch (error) {
         throw new Error(`Failed to get location from IP: ${error.message}`);
     }
